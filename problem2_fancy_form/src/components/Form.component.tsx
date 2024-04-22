0aabea07793e4b9e619f3cbbc163662727fd487a
huỳnh bá vinh <55732546+huynhbavinh@users.noreply.github.com>
2024-04-22 20:21:33 +0700

    update from component

--- problem2_fancy_form/src/components/Form.component.tsx
+++ problem2_fancy_form/src/components/Form.component.tsx
@@ -6,15 +6,24 @@ import { useEffect, useState } from "react";
 import { Currency } from "../types/currency.type";
 import { removeDuplicateValue } from "../utils";
 import './From.component.css'
+import { NumericFormat } from 'react-number-format';
+import InputAdornment from '@mui/material/InputAdornment'
+
+function CustomTextField(errFromValue: boolean) {
+    return (
+        <TextField sx={{ display: 'inline-block' }} id="value-to" label="Value" variant="filled"
+            error={errFromValue}
+            helperText={errFromValue ? 'Invalid Input Number' : ''}
+        />
+    )
+}
 
 export default function FormComponent() {
     const [listCurrency, setListCurrency] = useState<Array<Currency>>([]);
     const [toCurrency, setToCurrency] = useState<Currency | null>(null);
-    const [fromValue, setFromValue] = useState<string>('');
-    const [toValue, setToValue] = useState<string>('');
+    const [fromValue, setFromValue] = useState<number>(0);
 
     const [errFromValue, setErrFromValue] = useState<boolean>(false);
-    const [errToValue, setErrToValue] = useState<boolean>(false);
 
     useEffect(() => {
         const getListCurrencyFunc = async () => {
@@ -27,11 +36,9 @@ export default function FormComponent() {
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if (isNaN(parseInt(fromValue))) {
+        if (!fromValue) {
             setErrFromValue(true)
             return;
-        } else {
-
         }
     }
 
@@ -40,11 +47,26 @@ export default function FormComponent() {
             <Box sx={{ textAlign: 'center' }}>
                 <Box component={'h1'}>Form Currency Swap</Box>
             </Box>
-            <Box sx={{display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+            <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
                 <Box className='input-control'>
-                    <Box component={'h4'}>Form Currency:</Box>
-                    <TextField sx={{ display: 'inline-block' }} id="value-from" label="Value" variant="filled"
-                        onChange={(e) => { setFromValue(e.currentTarget.value) }}
+                    <Box component={'h4'} paddingBottom={'1rem'}>Form Currency:</Box>
+                    <NumericFormat
+                        label="Currency from"
+                        value={fromValue}
+                        customInput={TextField}
+                        variant="filled"
+                        error={errFromValue}
+                        helperText={errFromValue ? 'Invalid number' : ''}
+                        InputProps={{
+                            startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                        }}
+                        allowNegative={false}
+                        thousandSeparator
+                        placeholder="0"
+                        onValueChange={({ value }) => {
+                            setFromValue(Number(value));
+                            setErrFromValue(false);
+                        }}
                     />
                     <TextField sx={{ display: 'inline-block', width: '10rem' }} className="disableText" label="Value" variant="outlined"
                         value={'USD'}
@@ -54,14 +76,9 @@ export default function FormComponent() {
                     />
                 </Box>
                 <Box className='input-control'>
-                    <Box component={'h4'}>Form Currency:</Box>
-                    <TextField sx={{ display: 'inline-block' }} id="value-to" label="Value" variant="filled"
-                        onChange={(e) => { setToValue(e.currentTarget.value) }}
-                        error={errFromValue}
-                        helperText={errFromValue ? 'Invalid Input Number' : ''}
-                    />
+                    <Box component={'h4'} paddingBottom={'1rem'}>To Currency:</Box>
                     <Autocomplete
-                        sx={{ display: 'inline-block', width: '10rem' }}
+                        sx={{ display: 'inline-block', width: '24rem' }}
                         id={'toCurrencyInput'}
                         disablePortal
                         options={listCurrency}
@@ -73,9 +90,9 @@ export default function FormComponent() {
                                     width="20"
                                     srcSet={`https://raw.githubusercontent.com/Switcheo/token-icons/main/tokens/${option.currency}.svg`}
                                     src={`https://raw.githubusercontent.com/Switcheo/token-icons/main/tokens/${option.currency}.svg`}
-                                    alt="Icon Currency"
+                                    alt="Icon"
                                 />
-                                {option.currency}
+                                {option.currency} - Price: {option.price}
                             </Box>
                         )}
                         renderInput={(params) => <TextField
@@ -90,8 +107,8 @@ export default function FormComponent() {
                     />
                 </Box>
             </Box>
-            <Box sx={{display: 'flex', justifyContent: 'center'}}>
-                <Button sx={{width: '10rem'}} type="submit" variant="outlined">Submit</Button>
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Button sx={{ width: '10rem' }} type="submit" variant="outlined">Submit</Button>
             </Box>
         </form>
     )
